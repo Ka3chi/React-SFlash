3783f477ed5bc30a3b7f4cd48e120507f732a0a2
bicol1community <bicol1community1@gmail.com>
2024-01-30 10:32:19 +0800

    try

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import AllRoutes from './Pages/Routes/AllRoutes'
 import "./index.css"
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 const App = () => {
   return (
     <div>
+      <ParallaxProvider>
       <AllRoutes/>
+      </ParallaxProvider>
     </div>
   )
 }
--- src/Pages/Partial/Dashboard.jsx
+++ src/Pages/Partial/Dashboard.jsx
@@ -1,13 +1,28 @@
-import Header from '../../Components/Header-Footer/Header'
-import Footer from '../../Components/Header-Footer/Footer'
+import Header from '../../Components/Header-Footer/Header';
+import Footer from '../../Components/Header-Footer/Footer';
+import { ParallaxBanner } from 'react-scroll-parallax';
+import img1 from '../../Assets/home1.jpg';
+import img2 from '../../Assets/welcome.png'
 
 export const Dashboard = () => {
   return (
     <div className="grid  grid-cols h-[1500px] w-screen overflow-x-hidden">
+      <ParallaxBanner
+      layers={[
+        { image: img1, speed: -20 },
+        { image: img2, speed: -10 },
+      ]}
+      className="aspect-[2/1]"
+    >
         <Header/>
-        {/* Image here */}
+      
+      <div className="absolute inset-0 flex items-center justify-center">
+        <h1 className="text-8xl text-white font-thin">Hello World!</h1>
+      </div>
+    </ParallaxBanner>
         <div className='home bg-violet-500 h-[800px] flex justify-center items-center' >
         </div>
+        {/* Image here */}
         <div className=" bg-sky-500 h-[773px] p-6 ">
           {/* //grid start here */}
           <div className="grid grid-cols-4 gap-5 h-[600px] w-[1490px] mx-[20px] mb-[20px] p-[20px] mt-[90px]">
